b9cd328f1ac5202a15eaff36af70c2bf6b771848
eustaquio <eustaquio.galano@gmail.com>
2024-12-04 12:23:35 +0800

    Change the overall approach for the displaying digits

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const buttonsContainer = document.querySelector('#buttons-container');
+function displayClickedNumber(element) {
+    
+    // foe debugging purposes
+    console.log("here" , element);
+
+    // for multiple digits
+    digits += element.textContent;
+
+    // display the clicked number
+    displayContainer.textContent = digits;
+
+}
+
+// START
+
+// get all the necessary elements in html 
 const displayContainer = document.querySelector('#display > div');
+const listOfNumberButtons = document.querySelectorAll('.number');
+let digits = ''; 
 
+// convert the node list of number buttons to an array
+const arrayOfNumberButtons = Array.from(listOfNumberButtons);
 
-buttonsContainer.addEventListener('click', (event) => {
-    const clickedButton = event.target;
-
-    console.log(clickedButton.innerText);
-
-    displayContainer.textContent = clickedButton.textContent
-    
-    
+// in each array element put an event listener
+// each item is a button element in html
+arrayOfNumberButtons.forEach((item) => {
+    item.addEventListener('click', (event) => {   
+        displayClickedNumber(event.target);
+    });
 });
